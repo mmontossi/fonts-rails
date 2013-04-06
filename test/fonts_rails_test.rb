f7d6c90a84e573f3f0e32a8146185f0bd56c6925
Mathías Montossi <mmontossi@mattways.com>
2013-04-06 15:26:34 -0300

    Small texts changes

--- test/fonts_rails_test.rb
+++ test/fonts_rails_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class FontsRailsTest < ActiveSupport::TestCase
 
-  test 'truth' do
+  test "truth" do
     assert_kind_of Module, Fonts::Rails
   end
 
--- test/helpers_test.rb
+++ test/helpers_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HelpersTest < ActionDispatch::IntegrationTest
 
-  test 'should add google fonts tag' do
+  test "should add google fonts tag" do
     get '/'
     assert_tag :tag => 'link', :attributes => { :href => 'http://fonts.googleapis.com/css?family=Open+Sans:300' }
   end

2d3fd3c01ccafb35e8000aa720e99ab258aa2a98
Mathías Montossi <mmontossi@mattways.com>
2013-08-05 16:05:20 -0300

    Forgot new 1.9 ruby hash syntax in tests

--- test/link_tag_test.rb
+++ test/link_tag_test.rb
@@ -4,7 +4,8 @@ class HelpersTest < ActionDispatch::IntegrationTest
 
   test "should add google fonts tag" do
     get '/'
-    assert_tag :tag => 'link', :attributes => { :href => 'http://fonts.googleapis.com/css?family=Open+Sans:300' }
+    assert_response :success, 'request fail'
+    assert_tag tag: 'link', attributes: { href: 'http://fonts.googleapis.com/css?family=Open+Sans:300' }, 'no link tag'
   end
 
 end

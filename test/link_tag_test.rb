1f7b9e954fe332fbde5cce521a0f48d8f26ffd56
Mathías Montossi <mmontossi@mattways.com>
2013-08-05 17:42:41 -0300

    Minor change to normalize tests style

--- test/link_tag_test.rb
+++ test/link_tag_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class LinkTagTest < ActionDispatch::IntegrationTest
 
-  test "should add google fonts tag" do
+  test "should show google fonts tag" do
     get '/'
     assert_response :success
     assert_tag tag: 'link', attributes: { href: 'http://fonts.googleapis.com/css?family=Open+Sans:300' }

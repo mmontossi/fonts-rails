c3515f4b20be3fe39bd370175c314be7488469d6
Mathías Montossi <mmontossi@museways.com>
2014-05-06 10:53:52 -0300

    Better test coverage

--- test/link_tag_test.rb
+++ test/link_tag_test.rb
@@ -1,11 +1,10 @@
 require 'test_helper'
 
-class LinkTagTest < ActionDispatch::IntegrationTest
+class LinkTagTest < ActionView::TestCase
 
-  test "should show google fonts tag" do
-    get '/'
-    assert_response :success
-    assert_tag tag: 'link', attributes: { href: 'http://fonts.googleapis.com/css?family=Open+Sans:300' }
+  test "should return include tag with url" do
+    self.request = OpenStruct.new(protocol: 'http://')
+    assert_equal '<link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css" />', google_fonts_link_tag('Open+Sans:300')
   end
 
 end

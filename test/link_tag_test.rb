6f24e031f35b8c136de6403c1560d1afba2c262f
Mathías Montossi <mmontossi@museways.com>
2014-09-16 20:50:58 -0300

    Remove double quotes

--- test/link_tag_test.rb
+++ test/link_tag_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class LinkTagTest < ActionView::TestCase
 
-  test "return include tag with url" do
+  test 'return include tag with url' do
     self.request = OpenStruct.new(protocol: 'http://')
     assert_equal(
       '<link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css" />',

af90e9b22f4d05d0946a798d80e916a50387e5be
Mathías Montossi <mmontossi@museways.com>
2017-06-26 22:29:47 -0300

    Rails 5.1 compatibility

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
-class ViewTest < ActionView::TestCase
+class HelperTest < ActionView::TestCase
+  include Fonts::Rails::Extensions::ActionView::Base
 
   test 'include tag' do
-    self.request = ActionDispatch::TestRequest.new
+    self.request = ActionDispatch::TestRequest.new({})
+
     assert_equal(
       '<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300" />',
       google_fonts_link_tag('Open+Sans:300')
